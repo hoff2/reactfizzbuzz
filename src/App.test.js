0c425625517f7da5cd6cb5031bbfe935b789129c
Charles Hoffman <choffman@pillartechnology.com>
2019-01-04 13:27:04 -0600

    some preliminary tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {shallow, mount, render} from 'enzyme';
+import App from "./App";
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('has an input', () => {
+  const fizzBuzzApp = shallow(<App />);
+  expect(fizzBuzzApp.exists('input')).toBe(true);
+});
+
+it('has an input that can be typed in', () => {
+    const fizzBuzzApp = render(<App />);
+    const theInput = fizzBuzzApp.find('input').first();
+    theInput.value = 'some text';
+    expect(theInput.value).toBe('some text');
+});
+
