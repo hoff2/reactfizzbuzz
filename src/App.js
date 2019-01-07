6cb7a25c38bd4ba61336bfc0cd5de7bd5b0590e3
Charles Hoffman <choffman@pillartechnology.com>
2019-01-07 15:29:25 -0600

    tests for UI elements, fizzbuzz logic, and an experiment

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+    calculate;
+
     constructor(props) {
         super(props);
+        this.calculate = props.calculate;
     }
 
     render() {
         return (
             <div className="App">
+                <input type="text" />
+                <button onClick={this.calculate}>Click Me!</button>
+                <div id="output" />
             </div>
         );
     }
+}
+
+export function calculateFizzBuzz(n) {
+    if(n % 15 === 0) {
+        return 'fizzbuzz';
+    }
+    if(n % 3 === 0) {
+        return 'fizz';
+    }
+    if (n % 5 === 0) {
+        return 'buzz';
+    }
+    return n;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {shallow, mount, render} from 'enzyme';
-import App from "./App";
+import App, {calculateFizzBuzz} from "./App";
 
-it('Responds 1 given 1', () => {
-    const fizzBuzzApp = mount(<App/>);
+describe('fizzbuzz app', () => {
+
+    let fizzBuzzApp;
+
+    beforeEach(() => {
+        fizzBuzzApp = shallow(<App />);
+    });
+
+    it('has a text input', () => {
+        expect(fizzBuzzApp.exists('input[type="text"]')).toBe(true);
+    });
+
+    it('has a button', () => {
+        expect(fizzBuzzApp.exists('button')).toBe(true);
+    });
+
+    it('has a div for results to show up in', () => {
+        expect(fizzBuzzApp.exists('div#output')).toBe(true);
+    });
 
 });
 
+describe('calculateFizzBuzz function', () => {
+    it('returns 1 given 1', () => {
+        expect(calculateFizzBuzz('1')).toBe('1');
+    });
 
+    it('returns 2 given 2', () => {
+        expect(calculateFizzBuzz('2')).toBe('2');
+    });
+
+    it('returns fizz given multiples of 3', () => {
+        expect(calculateFizzBuzz('3')).toBe('fizz');
+        expect(calculateFizzBuzz('6')).toBe('fizz');
+        expect(calculateFizzBuzz('33')).toBe('fizz');
+    });
+
+    it('returns buzz given multiples of 5', () => {
+        expect(calculateFizzBuzz('5')).toBe('buzz');
+        expect(calculateFizzBuzz('10')).toBe('buzz');
+        expect(calculateFizzBuzz('55')).toBe('buzz');
+    });
+
+    it('returns fizzbuzz given multiples of 3 and 5', () => {
+        expect(calculateFizzBuzz('15')).toBe('fizzbuzz');
+        expect(calculateFizzBuzz('30')).toBe('fizzbuzz');
+        expect(calculateFizzBuzz('165')).toBe('fizzbuzz');
+    });
+});
+
+describe('fizzbuzz app with function', () => {
+    let fizzBuzzApp;
+
+    const justLogSomething = (n) => {
+        console.log(`Something! ${n}`);
+        return n;
+    };
+
+    beforeEach(() => {
+        fizzBuzzApp = shallow(<App calculate={justLogSomething} />);
+    });
+
+    it('whatever', () => {
+        fizzBuzzApp.find('button').simulate('click');
+    })
+});
 
