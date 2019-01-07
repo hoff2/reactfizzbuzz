b30d378f3b79f40c3ac2b6dd5810c1392b1684c6
Charles Hoffman <choffman@pillartechnology.com>
2019-01-07 14:38:31 -0600

    fresh start

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="App">
-                <input type="text" />
             </div>
         );
     }
--- src/App.test.js
+++ src/App.test.js
@@ -3,22 +3,10 @@ import ReactDOM from 'react-dom';
 import {shallow, mount, render} from 'enzyme';
 import App from "./App";
 
+it('Responds 1 given 1', () => {
+    const fizzBuzzApp = mount(<App/>);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
 
-it('has an input', () => {
-  const fizzBuzzApp = shallow(<App />);
-  expect(fizzBuzzApp.exists('input')).toBe(true);
-});
 
-it('has an input that can be typed in', () => {
-    const fizzBuzzApp = render(<App />);
-    const theInput = fizzBuzzApp.find('input').first();
-    theInput.value = 'some text';
-    expect(theInput.value).toBe('some text');
-});
 
